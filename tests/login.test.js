bef7d4ea2926b44060cf095a3aa9ce6e538c3dad
Victoria <vicmance@gmail.com>
2025-09-30 16:26:42 -0300

    Adicionando o arquivo inicial com teste de login

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,33 +1,32 @@
-import http from 'k6/http'
-import { sleep, check } from 'k6'
+import http from 'k6/http';
+import { sleep, check } from 'k6';
 
 export const options = {
-  iterations: 1,
-  thresholds: {
-    http_req_duration: ['p(90)<10']
-  }
+    iterations: 50,
+    thresholds: {
+    http_req_duration: ['p(90)<10','max<1'],
+    http_req_failed: ['rate<0.1']
+  },
 };
 
 export default function () {
-    const url = 'http://localhost:3000/login';
+  const url = 'http://localhost:3000/login';
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  });
 
-    const payload = JSON.stringify({
-        usarname: 'julio.lima',
-        senha: '123456',
-    });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-    const params = {
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    };
+  const res = http.post(url, payload, params);
+  check(res, {
+    'Validar que o status é 200': (r) => r.status === 200,
+    'Validar que o token é string': (r) => typeof(r.json().token)== 'string'
+  })
 
-    const res = http.post(url, payload, params);
-
-    check (res, {
-        'Validar que o Status é 200': (r) => r.status === 200,
-        'Validar que o token é string': (r) => typeof(r.json().token) == 'string'
-    })  
-
-    sleep(1)
-}
+  sleep(1);
+};
