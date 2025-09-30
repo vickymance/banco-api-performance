5395358748efb39556c97bc68b8384468384764c
Victoria <vicmance@gmail.com>
2025-09-30 17:32:48 -0300

    Adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,8 +2,11 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-    vus: 10,
-    duration: '30s',
+    stages: [
+    { duration: '5s', target: 10 },
+    { duration: '20s', target: 10 },
+    { duration: '5s', target: 0 },
+  ],
     thresholds: {
     http_req_duration: ['p(90)<3000','max<5000'],
     http_req_failed: ['rate<0.1']
